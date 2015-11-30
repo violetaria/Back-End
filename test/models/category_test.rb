1aa240b89fe6f00763e418729be8b1b7071e3026
violetaria <terri.chu@outlook.com>
2015-11-30 16:04:20 -0500

    initial model tests updated for user, category, and recipe

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,5 +4,27 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+  test "can_create_new_category" do
+    category = Category.new(name: "Test")
+
+    assert category.save
+    assert category.errors.blank?
+  end
+
+  test "cannot_create_category_without_name" do
+    category = Category.new()
+
+    refute category.save
+    assert category.errors.present?
+  end
+
+  test "cannot_create_two_categories_with_same_name" do
+    category = Category.new(name: "Test")
+    assert category.save
+    assert category.errors.blank?
+
+    category = Category.new(name: "Test")
+    refute category.save
+    assert category.errors.present?
+  end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -4,4 +4,49 @@ class RecipeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can_create_recipe" do
+    recipe = users(:one).recipes.new(name: "Test Recipe")
+
+    assert recipe.save
+    assert recipe.errors.blank?
+  end
+
+
+  test "cannot_create_recipe_without_user" do
+    recipe = Recipe.new(name: "Test Recipe")
+
+    refute recipe.save
+    assert recipe.errors.present?
+  end
+
+  test "cannot_create_recipe_without_name" do
+    recipe = users(:one).recipes.new()
+
+    refute recipe.save
+    assert recipe.errors.present?
+  end
+
+  test "one_user_cannot_create_recipe_with_same_name" do
+    recipe = users(:one).recipes.new(name: "Test Recipe")
+
+    assert recipe.save
+    assert recipe.errors.blank?
+
+    recipe = users(:one).recipes.new(name: "Test Recipe")
+    refute recipe.save
+    assert recipe.errors.present?
+  end
+
+  test "two_users_can_create_recipe_with_same_name" do
+    recipe = users(:one).recipes.new(name: "Test Recipe")
+
+    assert recipe.save
+    assert recipe.errors.blank?
+
+    recipe = users(:two).recipes.new(name: "Test Recipe")
+    assert recipe.save
+    assert recipe.errors.blank?
+  end
+
 end
