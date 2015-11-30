0e7475446aa89ac365eaa970b6aab31eef953139
violetaria <terri.chu@outlook.com>
2015-11-30 11:13:28 -0500

    routes for new and create for users

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  post "/users", to: "users#create"
+  post "users/new", to: "users#new"
+
   # Example resource route with options:
   #   resources :products do
   #     member do
