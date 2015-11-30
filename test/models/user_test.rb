f1d35beff787768e84ce6a9c863087a7f2b4d82a
violetaria <terri.chu@outlook.com>
2015-11-30 18:46:38 -0500

    updated fixgtures and updated user test names

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "can_create_user" do
+  test "can create user" do
     user = User.new(email: "test@user.com",
                     password: "password")
 
@@ -13,21 +13,21 @@ class UserTest < ActiveSupport::TestCase
     assert user.errors.empty?
   end
 
-  test "cannot_create_user_without_email" do
+  test "cannot create user without email" do
     user = User.new(email: "test@user.com")
 
     refute user.save
     assert user.errors.present?
   end
 
-  test "cannot_create_user_without_password" do
+  test "cannot create user without password" do
     user = User.new(password: "password")
 
     refute user.save
     assert user.errors.present?
   end
 
-  test "cannot_create_user_with_invalid_email" do
+  test "cannot create user with invalid email" do
     user = User.new(email: "test@",
                     password: "password")
 
@@ -35,14 +35,8 @@ class UserTest < ActiveSupport::TestCase
     assert user.errors.present?
   end
 
-  test "cannot_create_two_users_with_same_email" do
-    user = User.new(email: "test@user.com",
-                    password: "password")
-
-    assert user.save
-    assert user.errors.empty?
-
-    user = User.new(email: "test@user.com",
+  test "cannot create two users with same email" do
+    user = User.new(email: users(:one).email,
                     password: "password")
 
     refute user.save
