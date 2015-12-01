c88d64392ceffd03d87c6ec0006a2f680f7a39da
violetaria <terri.chu@outlook.com>
2015-12-01 17:01:20 -0500

    migration for directions

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201015440) do
+ActiveRecord::Schema.define(version: 20151201214205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "directions", force: :cascade do |t|
+    t.integer  "recipe_id",  null: false
+    t.string   "step",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
