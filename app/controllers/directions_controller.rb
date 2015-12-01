52138b20240bfbbcd8c3f158bea203071198291a
violetaria <terri.chu@outlook.com>
2015-12-01 18:18:19 -0500

    updated category view to show directions, udpated config routes for future use of directions, initial directions controller, will come back later to look

---
+++ app/controllers/directions_controller.rb
@@ -0,0 +1,14 @@
+class DirectionsController < ApplicationController
+  before_action :authenticate_user!
+  #
+  # def update
+  #   recipe = current_user.recipes.find_by!(id: params[:recipe_id])
+  #
+  #   recipe.directions
+  # end
+  #
+  # def destroy
+  #
+  # end
+
+end
--- app/views/categories/show.json.jbuilder
+++ app/views/categories/show.json.jbuilder
@@ -4,5 +4,6 @@ json.recipes do
     json.id recipe.id
     json.name recipe.name
     json.categories recipe.category_names
+    json.directions recipe.steps
   end
 end
