c06962f79c0109fd59de6a78f5934b0cdb8e83e2
violetaria <terri.chu@outlook.com>
2015-12-03 13:50:24 -0500

    updated model validation to not include unit

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,6 +2,6 @@ class RecipeIngredient < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
 
-  validates_presence_of :amount, :unit
+  validates_presence_of :amount
   validates_uniqueness_of :amount, :unit, scope: [:recipe_id, :ingredient_id]
 end
