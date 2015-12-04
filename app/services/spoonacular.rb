4a0556d19d291881c90fe59e52921e309ce8e8a9
violetaria <terri.chu@outlook.com>
2015-12-04 11:05:18 -0500

    api for hooking to the food api service

---
+++ app/services/spoonacular.rb
@@ -0,0 +1,40 @@
+#module FlourPower
+  class Spoonacular
+    include HTTParty
+    base_uri "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+    def initialize()
+      @auth = {
+          "X-Mashape-Key" => ENV["SPOONACULAR_KEY"]
+      }
+
+      @base_images_uri = "https://spoonacular.com/recipeImages/"
+    end
+
+    def search_recipes(query)
+      response = Spoonacular.get("/recipes/search?query=#{query}",
+                     headers: @auth)
+      image_base_uri = response.parsed_response["baseUri"]
+      binding.pry
+      response.parsed_response.map do |recipe|
+        Hash[:id, recipe["id"]]
+      end
+    end
+
+    def get_recipe_info(recipe_id)
+      response = Spoonacular.get("/recipes/#{recipe_id}/information",
+                                 headers: @auth)
+      data = response.parsed_response
+      {source_url: data["sourceUrl"],
+       source_name: data["sourceName"],
+       source_id: data["id"],
+       name: data["title"],
+       stock_img: @base_images_uri+data["image"]}
+    end
+
+    def get_recipe_data(source_url)
+      url_encoded = ERB::Util.url_encode(source_url)
+      Spoonacular.get("/recipes/extract?forceExtraction=false&url=#{url_encoded}",
+                      headers: @auth)
+    end
+  end
+#end
