75f45c07c1c4b942cc320b5154cdb43cc6a6baec
violetaria <terri.chu@outlook.com>
2015-12-07 11:29:43 -0500

    updated recipes controller so destroy has error checking and fixed up some spacing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -80,15 +80,11 @@ class RecipesController < ApplicationController
     else
       render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
     end
-    ## TODO add additional creates here once other models are created
   end
 
   def update
     recipe = current_user.recipes.find_by!(id: params[:id])
-
-    binding.pry
     recipe.update(recipe_params)
-
     if recipe.errors.blank?
       render json: { success: "true" }, status: :ok
     else
@@ -98,10 +94,12 @@ class RecipesController < ApplicationController
 
   def destroy
     recipe = current_user.recipes.find_by!(id: params[:id])
-
     recipe.destroy
-
-    render json: { success: "true" }, status: :ok
+    if recipe.errors.blank?
+      render json: { success: "true" }, status: :ok
+    else
+      render json: { errors: recipe.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
