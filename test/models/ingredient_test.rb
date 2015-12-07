9764a2f549a0d6513f9c961cce099008fc7d3ebd
violetaria <terri.chu@outlook.com>
2015-12-07 16:18:50 -0500

    ingredient model tests + update to user test

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create new ingredient" do
+    ingredient = Ingredient.new(name: "turkey jerky")
+
+    assert ingredient.save
+
+    assert ingredient.errors.blank?
+  end
+
+  test "cannot create two ingredients with same name" do
+    ingredient = Ingredient.new(name: ingredients(:one).name)
+
+    refute ingredient.save
+
+    assert ingredient.errors.present?
+  end
+
 end
