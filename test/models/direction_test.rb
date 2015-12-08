1c457f7bd91730b8dd3ec3caa56d0e8445f6ecf7
violetaria <terri.chu@outlook.com>
2015-12-08 10:07:43 -0500

    directions model tests added

--- test/models/direction_test.rb
+++ test/models/direction_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class DirectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create new direction" do
+    direction = recipes(:one).directions.new(step: "Test Step")
+
+    assert direction.save
+
+    assert direction.errors.blank?
+  end
+
+  test "cannot create same direction twice in a recipe" do
+    direction = recipes(:one).directions.new(step: "Test Step")
+    direction_dupe = recipes(:one).directions.new(step: "Test Step")
+
+    assert direction.save
+    refute direction_dupe.save
+
+    assert direction.errors.blank?
+    assert direction_dupe.errors.present?
+  end
+
+  test "cannot create direction without recipe" do
+    direction = Direction.new(step: "Test Step")
+
+    refute direction.save
+
+    assert direction.errors.present?
+  end
 end
