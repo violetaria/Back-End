d3dbe0e1855e960e8aa4e7c2d0459409838d4108
violetaria <terri.chu@outlook.com>
2015-12-08 10:03:05 -0500

    updated fixtures and added recipe ingredient model tests

--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -2,6 +2,32 @@ require 'test_helper'
 
 class RecipeIngredientTest < ActiveSupport::TestCase
   test "can create recipe ingredient" do
-    recipe_ingredient =
+    recipe_ingredient = recipes(:two).recipe_ingredients.new(ingredient_id: ingredients(:one).id, amount: 1.0, unit: "cups")
+
+    assert recipe_ingredient.save
+
+    assert recipe_ingredient.errors.blank?
+  end
+
+  test "cannot create duplicate records attached to one recipe" do
+    recipe_ingredient = recipes(:two).recipe_ingredients.new(ingredient_id: ingredients(:one).id, amount: 1.0, unit: "cups")
+    recipe_ingredient_dupe = recipes(:two).recipe_ingredients.new(ingredient_id: ingredients(:one).id, amount: 1.0, unit: "cups")
+
+    assert recipe_ingredient.save
+    refute recipe_ingredient_dupe.save
+
+    assert recipe_ingredient.errors.blank?
+    assert recipe_ingredient_dupe.errors.present?
+  end
+
+  test "can create duplciate record attached to two recipes" do
+    recipe_ingredient = recipes(:two).recipe_ingredients.new(ingredient_id: ingredients(:one).id, amount: 1.0, unit: "cups")
+    recipe_ingredient_dupe = recipes(:one).recipe_ingredients.new(ingredient_id: ingredients(:three).id, amount: 1.0, unit: "cups")
+
+    assert recipe_ingredient.save
+    assert recipe_ingredient_dupe.save
+
+    assert recipe_ingredient.errors.blank?
+    assert recipe_ingredient_dupe.errors.blank?
   end
 end
